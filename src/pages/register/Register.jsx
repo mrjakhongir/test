1a38066ebfdf42131fd0e7516767907dd9bb5d3f
Jahongir_Nusratov <nusratovjahongir@gmail.com>
2024-03-02 11:19:36 +0500

    feat: add roboto sans font family to the project. Create Register page layout

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,51 @@
+import { Link } from 'react-router-dom';
 import './register.scss';
 
 function Register() {
-	return <div>Register</div>;
+	return (
+		<div className='register'>
+			<div className='register__inner'>
+				<h1>Sign Up to Telegram</h1>
+				<p>Please enter your email and password.</p>
+				<form>
+					<fieldset>
+						<legend>Username</legend>
+						<input
+							type='text'
+							name='username'
+							id='username'
+							placeholder='Your username...'
+							reduired
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Email</legend>
+						<input
+							type='email'
+							name='email'
+							id='email'
+							placeholder='Your email...'
+							reduired
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Password</legend>
+						<input
+							type='password'
+							name='password'
+							id='password'
+							placeholder='Your password...'
+							reduired
+						/>
+					</fieldset>
+					<button>Sign Up</button>
+				</form>
+				<p>
+					Already have an account? <Link to='/signin'>Sign In</Link>
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
